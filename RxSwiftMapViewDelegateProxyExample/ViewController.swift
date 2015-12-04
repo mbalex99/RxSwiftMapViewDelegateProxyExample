f83d0953dbfa0dd7bd9038fa9843e60fde4d0460
Maximilian Alexander <mbalex99@gmail.com>
2015-12-04 09:47:39 -0800

    Update ViewController.swift

--- RxSwiftMapViewDelegateProxyExample/ViewController.swift
+++ RxSwiftMapViewDelegateProxyExample/ViewController.swift
@@ -40,12 +40,5 @@ class ViewController: UIViewController {
             .addDisposableTo(disposeBag)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
